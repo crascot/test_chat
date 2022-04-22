4ef487011b20b3d03a7e4fd8342d32904c2826ba
crascot <y-sitdikov@mail.ru>
2022-04-22 20:16:05 +0600

    first commit

--- src/index.js
+++ src/index.js
@@ -1,21 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
+import { createRoot } from 'react-dom/client';
+import 'antd/dist/antd.css';
 
-ReactDOM.render(
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
