b68a5d8b12a4e5032de4d8e39ed6ecf7d0671f85
crascot <y-sitdikov@mail.ru>
2022-06-08 01:09:41 +0600

    compleat fourth block

--- src/components/blocks/fourth-block/fourthBlockStyle.js
+++ src/components/blocks/fourth-block/fourthBlockStyle.js
@@ -1,21 +1,4 @@
 export const fourthBlockStyle = {
-    main: {
-        backgroundColor: 'white'
-    },
-    avatar: {
-        width: '15vw',
-        height: '15vw',
-        margin: '0 auto',
-    },
-    contactData: {
-        width: '100%'
-    },
-    name: {
-        textAlign: 'center',
-        marginTop: 25,
-        fontSize: '2.5vw'
-    },
-
     iconPhone: { color: '#0093E9' },
     iconVideo: { color: '#0093E9' },
     iconHeart: { color: '#ff0000' },
